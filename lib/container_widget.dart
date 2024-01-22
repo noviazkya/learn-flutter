c8ef2c04f5fed2b5aa001ab229e442547f211d0d
noviazkya <azkyanovi@gmail.com>
2024-01-22 14:42:48 +0700

    container

---
+++ lib/container_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+  class BelajarContainer extends StatelessWidget {
+    const BelajarContainer({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        width: double.infinity,
+        height: double.infinity,
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: Layerkedua()
+  
+      );
+    }
+  }
+
+   class Layerkedua extends StatelessWidget {
+    const Layerkedua({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        width: double.infinity,
+        height: double.infinity,
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.purple,
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: Layerketiga()
+            );
+    }
+  }
+  
+   class Layerketiga extends StatelessWidget {
+    const Layerketiga({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        width: double.infinity,
+        height: double.infinity,
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(10)
+        ),
+        child: Layerkeempat()
+      );
+    }
+  }
+
+     class Layerkeempat extends StatelessWidget {
+    const Layerkeempat({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        width: double.infinity,
+        height: double.infinity,
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+         image: DecorationImage(
+          image: 
+            NetworkImage("https://source.boomplaymusic.com/group10/M00/07/07/1c687ef773364925a3eae29bebb1fc37_464_464.jpg")
+         )
+        ),
+      );
+    }
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:belajar/container_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,13 +15,25 @@ class MyApp extends StatelessWidget {
       title: "belajar flutter pertama",
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.amber,
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 124, 77, 255),
           title: Text("Belajar Flutter"),
         ),
-        body: Center(
-          child: Text("hello world")
-          ),
+        body: BelajarContainer(),
       ),
     );
   }
 }
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("hello world")
+      );
+  }
+} 
